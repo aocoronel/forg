dcdab8e2b8f40a51ff6630bb56db187ba5ab8e8a
Augusto Coronel <aoc@getgoogleoff.me>
2025-08-14 15:43:43 -0300

    Rewrite the README file, and remove `install.sh`

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Granting executable permission to forg"
-chmod u+x forg
-echo "Moving files"
-mv ./config/forg.conf "$HOME/.local/share/" && echo "Moved config files to $HOME/.local/share/"
-mv ./src/forg "$HOME/.local/bin/" && echo "Moved script to $HOME/.local/bin/"
